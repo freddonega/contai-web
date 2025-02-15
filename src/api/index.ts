f2fc5d276a16b2b2a7bec2e46d8b4629ded54d01
Fred Donega <fred.wejam@gmail.com>
2025-02-14 22:27:24 -0300

    env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { useAuthStore } from "@/storage/authStorage";
 
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL,
 });
 
 api.interceptors.request.use((config) => {
--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,5 @@ export default defineConfig({
     alias: [{ find: "@", replacement: "/src" }],
   },
   plugins: [react()],
+  // ...outras configurações...
 });
